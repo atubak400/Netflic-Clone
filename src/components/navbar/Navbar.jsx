a03cf76941b5588376065937a437b6bd049425de
Kingsley Atuba <atubak40@gmail.com>
2021-07-13 15:44:13 +0100

    navbar component creation continues

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
+import { Notifications, Search } from '@material-ui/icons'
 import React from 'react'
 import './navbar.scss'
 
 const Navbar = () => {
-  return <div className='navbar'>Navbar</div>
+  return (
+    <div className='navbar'>
+      <div className='container'>
+        <div className='left'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png'
+            alt=''
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className='right'>
+          <Search />
+          <span>KID</span>
+          <Notifications />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
