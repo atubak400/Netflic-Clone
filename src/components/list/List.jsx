390e7d87e61bdd45909075e7cc7e65d04bd83c3f
Kingsley Atuba <atubak40@gmail.com>
2021-07-28 14:36:10 +0100

    building movies slider

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,5 +1,31 @@
-import React from 'react'
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from '@material-ui/icons'
+import './list.scss'
+import ListItem from '../listItem/ListItem'
 
 export default function List() {
-  return <div>list</div>
+  return (
+    <div className='list'>
+      <span className='listTitle'>Continue to watch @ 54:17</span>
+      <div className='wrapper'>
+        <ArrowBackIosOutlined className='sliderArrow left' />
+        <div className='container'>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <ArrowForwardIosOutlined className='sliderArrow right' />
+      </div>
+    </div>
+  )
 }
