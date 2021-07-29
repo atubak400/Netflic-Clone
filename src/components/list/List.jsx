17a52fbc6c91dab2ade5b99196e5278a1ef12744
Kingsley Atuba <atubak40@gmail.com>
2021-07-29 16:58:07 +0100

    still building movies slider-implemented left and right scroll

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,14 +4,29 @@ import {
 } from '@material-ui/icons'
 import './list.scss'
 import ListItem from '../listItem/ListItem'
+import { useRef } from 'react'
 
 export default function List() {
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if (direction === 'left') {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`
+    }
+    if (direction === 'right') {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`
+    }
+  }
   return (
     <div className='list'>
-      <span className='listTitle'>Continue to watch @ 54:17</span>
+      <span className='listTitle'>Continue to watch</span>
       <div className='wrapper'>
-        <ArrowBackIosOutlined className='sliderArrow left' />
-        <div className='container'>
+        <ArrowBackIosOutlined
+          className='sliderArrow left'
+          onClick={() => handleClick('left')}
+        />
+        <div className='container' ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -24,7 +39,10 @@ export default function List() {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className='sliderArrow right' />
+        <ArrowForwardIosOutlined
+          className='sliderArrow right'
+          onClick={() => handleClick('right')}
+        />
       </div>
     </div>
   )
